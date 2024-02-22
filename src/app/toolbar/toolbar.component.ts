a0170ad52a1f65cca2b5b18c147dcc07b1cb7c1a
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2024-02-22 12:39:08 +0100

    added toolbar,user- and dashboard-routes

---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+  showFiller = false;
+
+
+}
